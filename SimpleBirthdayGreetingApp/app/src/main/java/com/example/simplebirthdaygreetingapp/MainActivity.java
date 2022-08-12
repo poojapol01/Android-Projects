cfd9c3989a9ed78bca6e1587eadcef5eb5f51e2e
poojapol01 <polpooja05@gmail.com>
2022-08-12 21:35:30 +0530

    Added button click event

--- SimpleBirthdayGreetingApp/app/src/main/java/com/example/simplebirthdaygreetingapp/MainActivity.java
+++ SimpleBirthdayGreetingApp/app/src/main/java/com/example/simplebirthdaygreetingapp/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.simplebirthdaygreetingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText eText;
+    Button birthdayGreetingBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        eText = (EditText)findViewById(R.id.nameInput);
+        birthdayGreetingBtn = (Button)findViewById(R.id.createBirthdayCardButton);
+
+        birthdayGreetingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String str = eText.getText().toString();
+                Toast.makeText(getBaseContext(), "Button is clicked and Name is: " +str, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }

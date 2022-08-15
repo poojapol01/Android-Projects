7f884346e97708a717e2690bd04ded9c3afa7920
poojapol01 <polpooja05@gmail.com>
2022-08-15 19:10:54 +0530

    Moving to next Activity using Intent

--- SimpleBirthdayGreetingApp/app/src/main/java/com/example/simplebirthdaygreetingapp/MainActivity.java
+++ SimpleBirthdayGreetingApp/app/src/main/java/com/example/simplebirthdaygreetingapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.simplebirthdaygreetingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -24,8 +25,10 @@ public class MainActivity extends AppCompatActivity {
         birthdayGreetingBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String str = eText.getText().toString();
-                Toast.makeText(getBaseContext(), "Button is clicked and Name is: " +str, Toast.LENGTH_SHORT).show();
+//                String str = eText.getText().toString();
+//                Toast.makeText(getBaseContext(), "Button is clicked and Name is: " +str, Toast.LENGTH_SHORT).show();
+                  Intent intent = new Intent(MainActivity.this, BirthdayGreetingActivity.class);
+                  startActivity(intent);
             }
         });
     }

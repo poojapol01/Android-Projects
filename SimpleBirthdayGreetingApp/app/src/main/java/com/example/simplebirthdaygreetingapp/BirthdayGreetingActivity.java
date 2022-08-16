1caa7fa7db1724a1c67e136bc4f8b05cc35421de
poojapol01 <polpooja05@gmail.com>
2022-08-16 17:42:54 +0530

    Modified BirthdayGreetingActivity UI

--- SimpleBirthdayGreetingApp/app/src/main/java/com/example/simplebirthdaygreetingapp/BirthdayGreetingActivity.java
+++ SimpleBirthdayGreetingApp/app/src/main/java/com/example/simplebirthdaygreetingapp/BirthdayGreetingActivity.java
@@ -10,10 +10,11 @@ public class BirthdayGreetingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_birthday_greeting);
 
         textView = findViewById(R.id.birthdayGreetingTextView);
         String receivedData = getIntent().getStringExtra("message_key");    //receive the value by getStringExtra() method and key must be same which is send by first activity
-        textView.setText("Happy Birthday " +receivedData);
+        textView.setText("Happy Birthday \n" +receivedData);
     }
 }

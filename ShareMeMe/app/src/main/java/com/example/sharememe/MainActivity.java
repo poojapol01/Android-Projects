3b81784888ab2b12ea8039386408841a9107cc70
poojapol01 <polpooja05@gmail.com>
2022-08-17 18:19:38 +0530

    Added ShareMeMe UI with Buttons

--- ShareMeMe/app/src/main/java/com/example/sharememe/MainActivity.java
+++ ShareMeMe/app/src/main/java/com/example/sharememe/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.sharememe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void ShareMeme(View view) {
+    }
+
+    public void NextMeme(View view) {
     }
 }

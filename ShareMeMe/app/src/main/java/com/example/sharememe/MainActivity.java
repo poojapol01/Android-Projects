61ad25f34693db876af8eb5c41247d5e749ceb06
poojapol01 <polpooja05@gmail.com>
2022-08-18 00:45:39 +0530

    Added Volley stringRequest implementation

--- ShareMeMe/app/src/main/java/com/example/sharememe/MainActivity.java
+++ ShareMeMe/app/src/main/java/com/example/sharememe/MainActivity.java
@@ -3,14 +3,55 @@ package com.example.sharememe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getName();
+    //private Button btnRequest;
+
+    private RequestQueue mRequestQueue;
+    private StringRequest mStringRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LoadMeme();
+    }
+    private void LoadMeme(){
+        String url = "https://meme-api.herokuapp.com/gimme";
+        //RequestQueue initialized
+        mRequestQueue = Volley.newRequestQueue(this);
+
+        //String Request initialized
+        mStringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                Toast.makeText(getApplicationContext(),"Response :" + response.toString(), Toast.LENGTH_LONG).show();//display the response on screen
+                Log.d(TAG,"Response :" + response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                Log.i(TAG,"Error :" + error.toString());
+            }
+        });
+
+        mRequestQueue.add(mStringRequest);
     }
 
     public void ShareMeme(View view) {
